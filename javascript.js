55ec5ba58f2a4ac118465698a9439cf5faf2e41c
Administrator <user@kali.localdomain>
2023-01-08 23:22:53 +0800

    chu

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function q(a){
+    document.getElementById(a).value="";
+}
+function jia(){
+    var a,b;
+    a=parseFloat(document.getElementById("n1").value);
+    b=parseFloat(document.getElementById("n2").value);
+    document.getElementById("n3").value=a+b;
+}
+function jian(){
+    var a,b;
+    a=parseFloat(document.getElementById("n1").value);
+    b=parseFloat(document.getElementById("n2").value);
+    document.getElementById("n3").value=a-b;
+}
+function cheng(){
+    var a,b;
+    a=parseFloat(document.getElementById("n1").value);
+    b=parseFloat(document.getElementById("n2").value);
+    document.getElementById("n3").value=a*b;
+}
+function chu(){
+    var a,b;
+    a=parseFloat(document.getElementById("n1").value);
+    b=parseFloat(document.getElementById("n2").value);
+    document.getElementById("n3").value=a/b;
+} 
