e58496ca90741552ad2c610e6626e77f308ea746
Administrator <2279772619@qq.com>
2023-01-09 13:19:16 +0800

    Revert "remete"
    This reverts commit 4c42b122513d844b4e47d81f365e3fc29b567c3f.

--- javascript.js
+++ javascript.js
@@ -24,9 +24,4 @@ function chu(){
     a=parseFloat(document.getElementById("n1").value);
     b=parseFloat(document.getElementById("n2").value);
     document.getElementById("n3").value=a/b;
-}
-function no(){
-    alert("那咋不对呢,就对!!!");
-    window.location.href = "https://github.com/jreeys-man";
-    window.location.href = "https://www.douyin.com/user/MS4wLjABAAAABWpMRo-ntWw5aHs6gAYfFhW-RSvMkVU0uGbVgsU70KsxG7cjymxZutgSIqdgJf430"
-}
+} 
